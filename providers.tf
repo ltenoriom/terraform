5897603e12f6d8bd1ed6cfec554b2497ad94954c
Luis Fernando Tenorio Mora <142266185+ltenoriom@users.noreply.github.com>
2024-03-24 21:22:44 -0600

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+
+
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "us-west-2"
+  shared_credentials_files = ["~/.aws/credentials"]
+
+}
